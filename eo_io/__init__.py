7163a358e05a55d9f58ed17bb38b41a75a4c4149
johnlavelle <git@lavelle.eu>
2022-05-11 15:41:19 +0100

    Modified import

--- eo_io/__init__.py
+++ eo_io/__init__.py
@@ -1,7 +1,7 @@
 #  Copyright (c) 2022.
 #  The ECHOES Project (https://echoesproj.eu/) / Compass Informatics
 
-from .core.settings import configuration
-from .core.storage.s3_interface import ReadWriteData
-from .core.tools import read_yaml
-from .core.storage import store_dataset, store_geotiff
+from eoian.core.settings import configuration
+from eoian.core.storage.s3_interface import ReadWriteData
+from eoian.core.tools import read_yaml
+from eoian.core.storage import store_dataset, store_geotiff
--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ def read_requirements(file):
         return f.read().splitlines()
 
 
-inst_reqs = read_requirements("requirements.txt")
+inst_reqs = read_requirements("./requirements.txt")
 
 setup(
     name='eo_io',
     version='0.1',
     packages=find_packages(exclude=["test"]),
     inst_reqs=inst_reqs,
+    setup_requires=inst_reqs,
     url='',
     license='',
     author='John Lavelle',
