a02861c252a19315cba2ecc8fbc9d422740415d5
johnlavelle <git@lavelle.eu>
2022-05-11 16:01:11 +0100

    Modified import

--- eo_io/__init__.py
+++ eo_io/__init__.py
@@ -1,7 +1,7 @@
 #  Copyright (c) 2022.
 #  The ECHOES Project (https://echoesproj.eu/) / Compass Informatics
 
-from eoian.core.settings import configuration
-from eoian.core.storage.s3_interface import ReadWriteData
-from eoian.core.tools import read_yaml
-from eoian.core.storage import store_dataset, store_geotiff
+from eo_io.core.settings import configuration
+from eo_io.core.storage.s3_interface import ReadWriteData
+from eo_io.core.tools import read_yaml
+from eo_io.core.storage import store_dataset, store_geotiff
