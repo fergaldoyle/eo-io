33954edc711927c8255b313e96412055a8f5c43e
John Lavelle <git@lavelle.eu>
2022-05-18 14:41:18 +0100

    Removed setup_requires from setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     version='0.1',
     packages=find_packages(exclude=["test"]),
     inst_reqs=inst_reqs,
-    setup_requires=inst_reqs,
     url='',
     license='',
     author='John Lavelle',
